7fec70a79000581be89bd472f629e090464e2aef
Ryan Lowe <ryanlowe@gmail.com>
2008-05-20 20:07:16 -0400

    disable sessions and unused Rails frameworks

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,9 +2,11 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
+  session :disabled => true
+  
+  #helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '4aee7f53dafb4aaddd9144c09b43aa2f'
+  #protect_from_forgery # :secret => '4aee7f53dafb4aaddd9144c09b43aa2f'
 end
